871a188dee6cb34f091294800fb9e03428cc2306
Aline <alinenaoe@gmail.com>
2020-01-17 12:05:53 -0300

    removendo tarefas e salvando informação no local storage

--- js/script.js
+++ js/script.js
@@ -16,41 +16,25 @@ show(tasksList);
 buttonAddTask.onclick = function () { // poderia ser com o addeventlistener
     let inputAddValue = inputAdd.value;
     tasksList.push(inputAddValue);
-
-    let taskElement = document.createElement('div');
-    taskElement.setAttribute('class', 'task'); //o setAttribute tira as classes que já estiverem no elemento
-
-    let taskName = document.createElement('div');
-    taskName.setAttribute('class', 'col-md-10');
-    taskName.textContent = inputAddValue;
-
-    let taskCheckbox = document.createElement('div');
-    taskCheckbox.setAttribute('class', 'col-md-2');
-
-    let imgCheck = document.createElement('img');
-    imgCheck.setAttribute('class', 'icon');
-    imgCheck.setAttribute('src', 'img/icon_check.png');
-
-    //depois de criar os elementos, agora insere um dentro do outro
-    taskCheckbox.appendChild(imgCheck); 
-    taskElement.appendChild(taskName);
-    taskElement.appendChild(taskCheckbox);
-    taskBoard.appendChild(taskElement);
-
+    createTask(inputAddValue, tasksList.length - 1)
     localStorage.setItem('tasksList', JSON.stringify(tasksList))
     //appendChild coloca elementos, não texto! texto é pelo textContent dentro de um elemento
 }
 
-
 function show (tasksList) { //copia valores de cima 
-    for(let item of tasksList) { //como o foreach tasks as task
-        createTask(item);
-    }
+    // for(let item of tasksList) { //como o foreach tasks as task
+    //     createTask(item);
+    taskBoard.innerHTML = "";
+    tasksList.forEach(function(value, position){
+        createTask(value, position)
+    })
+    // }
 } 
 
-function createTask (inputAddValue) {
+function createTask (inputAddValue, position) {
     let taskElement = document.createElement('div');
     taskElement.setAttribute('class', 'task'); 
+    taskElement.setAttribute('position', position); 
 
     let taskName = document.createElement('div');
     taskName.setAttribute('class', 'col-md-10');
@@ -62,9 +46,26 @@ function createTask (inputAddValue) {
     let imgCheck = document.createElement('img');
     imgCheck.setAttribute('class', 'icon');
     imgCheck.setAttribute('src', 'img/icon_check.png');
+    imgCheck.onclick = function (event) { //no momento que cria a tarefa você já diz que aquela imagem quando clicada resulta em uma ação
+        // console.log(tasksList);
+        let taskPosition = taskElement.getAttribute('position');
+        tasksList = tasksList.filter(function(value, position){
+            return position != taskPosition;
+        })
+        show(tasksList)
+        localStorage.setItem('tasksList', JSON.stringify(tasksList))
+
+        //console.log(taskPosition);
+        taskElement.remove();
+   
+    // OUTRA FORMA DE FAZER - SELECIONANDO PELO PAI
+    // let tarefaPai = event.target.parentNode.parentNode;
+    // tarefaPai.remove();
+    }
 
     taskCheckbox.appendChild(imgCheck); 
     taskElement.appendChild(taskName);
     taskElement.appendChild(taskCheckbox);
     taskBoard.appendChild(taskElement);
 }
+
